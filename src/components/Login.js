48d52431e92e6f6a1c6a8e2a422da95f96ff16d9
Carl <reyes.carlj@gmail.com>
2018-02-27 12:00:30 -0500

    Centered login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,17 +7,19 @@ class Login extends Component {
     return (
       <div>
         {user.token === '' ?
-            <div>
-              <h2>Login Form</h2>
-              <form onSubmit={handleLogin}>
-                <label>Email</label>
-                <input type='email' name='email' value={email} onChange={handleChange} />
-                <br />
-                <label>Password</label>
-                <input type='password' name='password' value={password} onChange={handleChange} />
-                <br />
-                <input type='submit' value='Submit' />
-              </form>
+            <div className='modal'>
+              <div className='modal-container'>
+                <form onSubmit={handleLogin}>
+                  <h2>Login</h2>
+                  <label>Email</label>
+                  <input type='email' name='email' value={email} onChange={handleChange} />
+                  <br />
+                  <label>Password</label>
+                  <input type='password' name='password' value={password} onChange={handleChange} />
+                  <br />
+                  <input type='submit' value='Submit' />
+                </form>
+              </div>
             </div>
             :
             <Redirect to='/' />
